8b3540821ef46189637d40eb8596be61d2358baa
RandyGoldsmith <rgoldshz@gmail.com>
2018-07-19 00:39:25 -0500

    finished list of chatrooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,12 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
+import './RoomList.css';
 
 
 class RoomList extends Component {
 	constructor(props) {
 		super(props);
+
+		this.roomsRef = this.props.firebase.database().ref('rooms');
 
 		this.state = {
 			rooms: []
@@ -11,20 +14,35 @@ class RoomList extends Component {
 		
 	}
 
-	//this.roomsRef = this.props.firebase.database().ref('rooms');
+	
 
 
-//componentDidMount() {
-//	this.roomsRef.on('child_added', snapshot => {
-//		console.log(snapshot);
-//	});
-//}
+componentDidMount() {
+	this.roomsRef.on('child_added', snapshot => {
+		const room = snapshot.val();
+		room.key = snapshot.key;
+		this.setState({ rooms: this.state.rooms.concat( room )});
+	});
+}
 
 
 	render() {
 		return (
-			<h1>Bloc Chat</h1>
-
+		<div>
+			<div className="container">
+				<aside className="left-side">
+					<h1>Bloc Chat</h1>
+			
+					<div className="chatroom-list">
+					{
+						this.state.rooms.map( (room, index ) =>
+							<h2 key={index}>{room.room}</h2>
+						)
+					}
+					</div>
+				</aside>
+			</div>	
+		</div>
 		)
 	}
 }
